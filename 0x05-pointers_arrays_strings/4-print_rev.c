6deea37822b2cd41997f2429d410eba770fc4932
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-09-19 14:16:52 -0700

    Print Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prtings a string in reverse
+ *
+ * @s: string paramter input
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		;
+	for (--i; i >= 0; --i)
+
+		_putchar(s[i]);
+	_putchar('\n');
+}
