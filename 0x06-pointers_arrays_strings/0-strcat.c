19ec5a86afb70b41daaec9ca2d7babf505311332
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-09-22 16:53:23 -0700

    Excercise 1 Concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j = 0;
+	int i, j;
+
+	i = 0;
 
 	while (dest[i])
 		i++;
 	for (j = 0; src[j] ; j++)
-		dest[c++] = src[j];
+		dest[i++] = src[j];
 
 	return (dest);
 }
