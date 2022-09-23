3195b3a41ece42c04359c148ac3511533b03fcac
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-09-22 21:30:05 -0700

    Ex1 - Comment Example

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,11 @@ int main(void)
 	printf("Infinte loop incoming :(\n");
 
 	i = 0;
+
 	/* while (i < 10)*/
-	 /* {
-	 * putchar(i);
-	 * }
-	 */
+	 /* {*/
+	/* putchar(i);*/
+	/* }*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
