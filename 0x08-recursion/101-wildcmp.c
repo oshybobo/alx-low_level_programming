55e3e809b3212b1ecdd227adb768779836f80a36
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-09-29 01:15:34 -0700

    Wild Card Exercise

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,61 @@
+/**
+ * substring_match - Function to compare strings and find match
+ * @s1: string param 1
+ * @s2: string param 2
+ * @after_wldcd: placeholder to determine
+ * Return: 1 if matched and 0 if not
+ */
+int substring_match(char *s1, char *s2, char *after_wldcd)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	else if (*s1 == '\0' && *s2 == '*')
+	{
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	}
+	else if (*s1 == '\0' && *s2 != '\0')
+	{
+		return (0);
+	}
+	else if (*s2 == '*')
+	{
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+	}
+	else
+	{
+		return (subtring_match(s1 + 1, after_wldcd, after_wldcd));
+	}
+}
+
+/**
+ * wildcmp - compare string with wildcard
+ * @s1: string param 1
+ * @s2: string param 2
+ * Return: 1 if match and 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else if (*s2 == '*')
+	{
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+	}
+	else
+	{
+		return (0);
+	}
+}
