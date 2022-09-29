a36770ff8ec288eb7cb76d3ecbdda28f090521ff
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-09-28 20:43:05 -0700

    Excercise length of String

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - functon returns string length
+ *
+ * @s: string parameter
+ *
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+}
