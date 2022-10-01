63234b4c116422587f4f4d36d070b1bd1895bb4d
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-09-30 20:29:40 -0700

    Exercise 1

---
+++ 0x0A-argc_agrv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point & print program name
+ * @argc: first arg and arg counter
+ * @argv: pointer to array of arg
+ *
+ * Return: 0 on succes
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
