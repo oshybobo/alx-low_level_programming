6c6b1d3c007d57bd52d9c2b468e4bff85747913d
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-10-04 19:28:35 -0700

    Excercise 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+/**
+ * *_strdup - duplicate string
+ * @str: input string
+ *
+ * Return: null if str = Null
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, j 0;
+	char *s;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	s = malloc((j + 1) * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+
+	}
+
+	return (s);
+}
+
