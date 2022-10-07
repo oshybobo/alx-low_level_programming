4de204fdb98fabc4b6f0156e1cc1bbb75f29ad1a
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-10-06 19:37:10 -0700

    Base files for Project

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef  MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+int _putchar(char c);
+
+
+
+#endif /* MAIN_H */
