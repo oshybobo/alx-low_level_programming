e6473ec420170c2c57e8dae97b9af7e12ad8edaa
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-10-06 19:58:20 -0700

    Memory allocation

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * *malloc_checked - allocates memory
  * @b: size to malloc
@@ -10,7 +10,7 @@ void *malloc_checked(unsigned int b)
 {
 	void *p;
 
-	p = malloc(a);
+	p = malloc(b);
 
 	if (p == NULL)
 	{
