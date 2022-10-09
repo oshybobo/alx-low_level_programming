d4b18d771baceac98a403fd35731eca3a18f6815
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-10-09 14:05:53 -0700

    Print 0-9 using Int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point - Function to print single digits in base 10 
+ * 		starting from 0
+ *
+ * Return: return 0 always
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
