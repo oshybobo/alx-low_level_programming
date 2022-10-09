cf25f2c47a3e072aac6777734764a7604dc22941
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-10-09 13:04:05 -0700

    Single Digit Print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point - function prints out all single digits of base 10
+ * 			starting from 0, followed by new line
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	char i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar('\n');
+	return (0);
+
+}
