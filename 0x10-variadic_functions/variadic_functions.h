188e0fe831b4a644d620a530f83efec70f2a3652
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-10-13 20:44:26 -0700

    Header File Update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int sum_them_all(const unsigned int n,...);
-void print_numbers(const char *separator, const unsigned n, ...);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
