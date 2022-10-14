747c1b544dfc1a1eefcd6607b75511da7c25d649
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-10-13 19:48:57 -0700

    First Commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include <stdargh.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns sum of param
+ * @n: integers to add
+ *
+ * Return: sum of all n
+ */
+
+int sum_them_all(cost unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int sum = 0, i;
+
+	if (n == 0)
+		return (0);
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
+
+	return (sum);
+}
