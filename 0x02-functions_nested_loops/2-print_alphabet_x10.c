62ab420766912447fc3bff9239edb73aadcd6077
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-11-15 14:17:31 -0800

    Updated Ex 2 and Main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times in lowercase
+ * 	followed by new line.
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	i = j = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
