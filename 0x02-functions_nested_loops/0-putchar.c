2150dd1af2e82074996cd40344bdd5f6f3c30d2e
Inumidun Oshinnaiye <oshinnaiye@gmail.com>
2022-11-15 13:21:02 -0800

    Update to 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+/**
+ * _putchar - function writes c to i/o
+ * @c: parameter
+ *
+ * Return: returns single c
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - print _putchar followed by new line
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
